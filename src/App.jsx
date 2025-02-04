494251dce9bf19116bac5a9567b44655fd71405f
Saadani Mohamed Amine <mohamedaminesaadani79@gmail.com>
2025-02-04 20:55:12 +0100

    feat: test hot reload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 const App = () => {
   return (
-    <div className="bg-blue-500 h-screen flex justify-center items-center">
+    <div className="bg-blue-500 h-screen flex flex-col text-white justify-center items-center">
        <h1 className="text-3xl font-bold underline text-white">Hello world!</h1>
+      <p>Thanks for development mode !!</p>
     </div>
   );
 };
