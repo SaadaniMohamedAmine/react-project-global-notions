321068318c464a1c8f459b09258d314caf31d0bb
Saadani Mohamed Amine <mohamedaminesaadani79@gmail.com>
2025-02-05 20:16:05 +0100

    feat: add the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import {Button} from "@material-tailwind/react"
+import Header from "./components/Header.jsx";
 
 const App = () => {
   return (
-    <div className="bg-blue-500 h-screen flex flex-col text-white justify-center items-center">
-       <h1 className="text-3xl font-bold underline text-white">Hello world!</h1>
-      <p>Thanks for development mode !!!</p>
-      <Button>Click ME</Button>
+    <div className="w-full">
+      <Header />
     </div>
   );
 };
