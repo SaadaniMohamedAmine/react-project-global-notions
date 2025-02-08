f4c7e146ee42fc209d49ae046ed3fe0cd031e7f2
Saadani Mohamed Amine <mohamedaminesaadani79@gmail.com>
2025-02-08 21:00:13 +0100

    feat: add the hero section in the home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,37 +1,35 @@
-import React from "react";
-
 const Hero = () => {
   return (
-    <div className="px-7 lg:px-10 max-w-6xl mx-auto flex flex-col gap-y-10 lg:flex-row items-center gap-x-10 justify-center py-10 lg:py-14 dark:bg-gray-800">
-      <div className="lg:w-[650px] lg:px-5 flex flex-col gap-y-5">
-        <h1 className="text-4xl md:text-5xl xl:text-[50px] leading-[1.2] md:max-w-xl md:mx-auto md:text-center lg:text-left lg:mx-0 lg:max-w-full font-semibold dark:text-white">
-          Discover the power of streamlined services
+    <div className="bg-neutral-950 text-white">
+      <div className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 py-20 ">
+        <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-white-300 dark:text-gray-300 sm:text-7xl">
+          Revolutionize Your Streamlined  
+          <span className="relative whitespace-nowrap text-white-600 dark:text-gray-300">
+            &nbsp; Services
+          </span>
+          <span className="relative whitespace-nowrap text-orange-500 dark:text-orange-300">
+            <svg
+              aria-hidden="true"
+              viewBox="0 0 418 42"
+              className="absolute top-2/3 left-0 h-[0.58em] w-full fill-orange-500 dark:fill-orange-300/60"
+              preserveAspectRatio="none"
+            >
+              <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.780 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.540-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.810 23.239-7.825 27.934-10.149 28.304-14.005 .417-4.348-3.529-6-16.878-7.066Z"></path>
+            </svg>
+            <span className="relative">with AI</span>
+          </span>
         </h1>
-        <p className="text-sm md:max-w-xl md:mx-auto lg:mx-0 lg:max-w-full md:text-center lg:text-left dark:text-gray-300">
-          With efficient services available online, you can simplify your tasks
-          and save valuable time.
-        </p>
-        <div className="flex gap-x-5 flex-col gap-y-2.5 lg:flex-row">
-          <a
-            href="#"
-            className="flex w-full lg:w-fit items-center text-white justify-center rounded-lg bg-[#1053F3] px-6 py-2.5 font-semibold hover:shadow-lg hover:drop-shadow transition duration-200"
-          >
-            <span>Get started</span>
-          </a>
-          <a
-            href="#"
-            className="flex w-full lg:w-fit items-center text-[#1053F3] justify-center rounded-lg border border-[#6A65FF] px-6 py-2.5 font-semibold hover:shadow-lg hover:drop-shadow transition duration-200 bg-[#5138EE]/10 dark:text-white dark:bg-transparent dark:border-gray-500/50"
-          >
-            <span>Learn more</span>
-          </a>
-        </div>
-      </div>
-      <div className="hero-image md:px-5 lg:px-0 w-full lg:w-1/2 rounded-3xl md:pt-2 lg:pt-0 relative isolate z-10">
-        <img
-          className="rounded-3xl w-full"
-          src="https://images.unsplash.com/photo-1590650516494-0c8e4a4dd67e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwxMnx8c2VydmljZXxlbnwwfDB8fHwxNzEyMjIyNjQ3fDA&ixlib=rb-4.0.3&q=80&w=1080"
-          alt=""
-        />
+        <h2 className="mx-auto mt-12 max-w-xl text-lg sm:text-white-400 text-white-500 dark:text-gray-300 leading-7">
+          Experience the future of Streamlined Services with our cutting-edge AI tool. From
+          underwriting to claims processing, our intelligent system streamlines
+          and enhances every aspect of your Services Operations.
+        </h2>
+        <a
+          className="bg-orange-600 dark:bg-gray-800 rounded-xl text-white dark:text-gray-300 font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-orange-500 dark:hover:bg-gray-600 transition"
+          href=""
+        >
+          Get started
+        </a>
       </div>
     </div>
   );
--- src/features/Home.jsx
+++ src/features/Home.jsx
@@ -1,13 +1,22 @@
-import React from "react";
 import Header from "../components/Header.jsx";
 import Hero from "../components/Hero.jsx";
+import Pricing from "../components/Pricing.jsx";
+import Services from "../components/Services.jsx"
+import Steps from "../components/Steps.jsx";
+import Testimonials from "../components/Testimonials.jsx";
+import Footer from "../components/Footer.jsx"
 
 const Home = () => {
   return (
-    <>
+    <div className="overflow-auto scrollbar-hide">
       <Header />
       <Hero />
-    </>
+      <Services />
+      <Steps />
+      <Testimonials />
+      <Pricing />
+      <Footer />
+    </div>
   );
 };
 
