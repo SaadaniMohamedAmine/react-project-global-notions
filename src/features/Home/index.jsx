eda4688fc6d71922fc968bcd2d4204d4e9cb7663
Saadani Mohamed Amine <mohamedaminesaadani79@gmail.com>
2025-02-09 18:19:52 +0100

    feat: update the architecture of the home page

--- src/features/Home/index.jsx
+++ src/features/Home/index.jsx
@@ -1,12 +1,22 @@
-import Layout from "../../components/Layout.jsx"
-import Hero from "../../components/Hero.jsx"
+import Layout from "../../components/Layout.jsx";
+import Hero from "../../components/Hero.jsx";
+import Pricing from "../../components/Pricing.jsx";
+import Services from "../../components/Services.jsx";
+import Steps from "../../components/Steps.jsx";
+import Testimonials from "../../components/Testimonials.jsx";
 
 const Home = () => {
   return (
     <Layout>
+      <div className="overflow-auto scrollbar-hide">
         <Hero />
+        <Services />
+        <Steps />
+        <Testimonials />
+        <Pricing />
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
