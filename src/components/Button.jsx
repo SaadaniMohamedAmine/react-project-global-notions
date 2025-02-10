2f81b2363f356692556c03298efb59d7f4ac6af7
Saadani Mohamed Amine <mohamedaminesaadani79@gmail.com>
2025-02-10 18:32:57 +0100

    feat: add the secondary button version

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,15 +5,22 @@ import PropTypes from "prop-types";
 const classes = (type) => {
   return clsx(
     type === "primary"
-      ? "!bg-orange-600 rounded-xl text-white font-medium px-4 py-3 transition cursor-pointer"
-      : ""
+      ? "!bg-orange-600 !hover:bg-orange-400 rounded-xl text-white font-medium px-4 py-3 transition cursor-pointer"
+      : "",
+    type === "secondary" ? "bg-transparent rounded-xl text-white font-medium px-4 py-3 transition cursor-pointer border border-orange-500" : ""
   );
+};
+
+const variants = {
+  primary: "filled",
+  secondary: "outlined",
+  tertiary: "text",
 };
 
 const MTButton = ({ children, type, size, onClick, disabled, fullWidth }) => {
   return (
     <MTBTN
-      variant="filled"
+      variant={`${variants[type]}`}
       size={size}
       onClick={onClick}
       className={`${classes(type)}`}
@@ -58,10 +65,30 @@ const PrimaryButton = ({ children, size, onClick, disabled, fullWidth }) => {
 PrimaryButton.propTypes = MTButton.propTypes;
 PrimaryButton.defaultProps = MTButton.defaultProps;
 
+const SecondaryButton = ({ children, size, onClick, disabled, fullWidth }) => {
+  // console.log("type is Primary");
+  return (
+    <MTButton
+      type="secondary"
+      size={size}
+      onClick={onClick}
+      disabled={disabled}
+      fullWidth={fullWidth}
+    >
+      {children}
+    </MTButton>
+  );
+};
+SecondaryButton.propTypes = MTButton.propTypes;
+SecondaryButton.defaultProps = MTButton.defaultProps;
+
 const Button = {
   Primary: PrimaryButton,
+  Secondary: SecondaryButton
 };
 
 Button.Primary = PrimaryButton;
+Button.Secondary = SecondaryButton;
+
 
 export default Button;
