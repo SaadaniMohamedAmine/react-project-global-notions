65344587a9ece7886723f4d78f08efde09e20676
Saadani Mohamed Amine <mohamedaminesaadani79@gmail.com>
2025-02-12 01:01:48 +0100

    feat: make the login modal based on search params

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./features/Home";
 import Docs from "./features/docs";
+import ModalContainer from "./features/services";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index path="/" element={<Home />} />
-        <Route path="/docs" element={<Docs />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <ModalContainer />
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="/docs" element={<Docs />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 export default App;
--- src/features/services/LoginModal.jsx
+++ src/features/services/LoginModal.jsx
@@ -1,21 +1,59 @@
-const LoginModal = () => {
+import PropTypes from "prop-types";
+import { IconButton } from "@material-tailwind/react";
+import { Modal } from "../../components/Modal";
+
+const serviceName = "login";
+
+const LoginModal = ({ service, handleOpen }) => {
+  console.log("Hello from the login modal");
   return (
-    <div>
-      <h1>Hello from the login modal </h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, animi
-        doloremque cupiditate maiores at, quasi minus quod rerum a porro, quia
-        iusto veritatis reprehenderit deleniti quam quisquam dolore earum
-        numquam?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, animi
-        doloremque cupiditate maiores at, quasi minus quod rerum a porro, quia
-        iusto veritatis reprehenderit deleniti quam quisquam dolore earum
-        numquam?
-      </p>
-    </div>
+    <Modal open={service === serviceName} handleOpen={handleOpen}>
+      <Modal.Header>
+        <div className="flex justify-between items-center">
+          <h1 className="text-bold text-xl text-black">
+            Hello from the login modal
+          </h1>
+          <IconButton
+            color="blue-gray"
+            size="sm"
+            variant="text"
+            onClick={handleOpen}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+              className="h-5 w-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </IconButton>
+        </div>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="mb-6">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt
+          aspernatur neque quas magni, obcaecati quaerat aliquam adipisci
+          veniam? Ea, saepe. Ab rerum accusantium nam, dicta voluptatibus
+          nostrum vitae id incidunt.
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <div>I am the footer</div>
+      </Modal.Footer>
+    </Modal>
   );
+};
+
+LoginModal.propTypes = {
+  service: PropTypes.string.isRequired,
+  handleOpen: PropTypes.func.isRequired,
 };
 
 export default LoginModal;
