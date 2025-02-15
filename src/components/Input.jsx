a73f2867ae3b82f735749502ebca07b215a471da
Saadani Mohamed Amine <mohamedaminesaadani79@gmail.com>
2025-02-15 11:11:50 +0100

    fix: add more props to the input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,16 @@
 import PropTypes from "prop-types";
 import { Input as MTInput } from "@material-tailwind/react";
 
-const Input = ({ label, size }) => {
+const Input = ({ label, size, value, onChange, type, success, error }) => {
   return (
     <MTInput
       label={label}
       size={size}
+      type={type}
+      value={value}
+      onChange={onChange}
+      success={success}
+      error={error}
       color="orange"
       className="border !border-orange-500 focus:!outline-0 focus:!border-t-0"
     />
@@ -14,11 +19,21 @@ const Input = ({ label, size }) => {
 
 Input.defaultProps = {
   size: "md",
+  value: "",
+  onChange: Function.prototype,
+  type: "text",
+  success: false,
+  error: false,
 };
 
 Input.propTypes = {
   label: PropTypes.string.isRequired,
   size: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  type: PropTypes.string,
+  success: PropTypes.bool,
+  error: PropTypes.bool,
 };
 
 export default Input;
