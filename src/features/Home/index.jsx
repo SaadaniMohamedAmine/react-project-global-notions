d5567f0af39386a1bb3566174802eb9a5fba160b
Saadani Mohamed Amine <66434181+SaadaniMohamedAmine@users.noreply.github.com>
2025-02-17 13:19:11 +0100

    Feat add title of the website (#14)
    * feat: add the package of react helmet
    
    * feat: change the title of the website
    
    ---------
    
    Co-authored-by: Saadani Mohamed Amine <mohamedaminesaadani79@gmail.com>

--- src/features/Home/index.jsx
+++ src/features/Home/index.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import Layout from "../../components/Layout.jsx";
 import Hero from "../../components/Hero.jsx";
 import Pricing from "../../components/Pricing.jsx";
@@ -8,6 +9,11 @@ import Testimonials from "../../components/Testimonials.jsx";
 const Home = () => {
   return (
     <Layout>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>StreamAIServ</title>
+        <link rel="icon" type="image/svg+xml" href="/icon-removebg-preview.png" />
+      </Helmet>
       <div className="overflow-auto scrollbar-hide">
         <Hero />
         <Services />
